205ae50ba59523320890c78f08e09ef26de1e47d
Nikscoder <nikita.zinchenko.2222@gmail.com>
2024-10-29 23:37:32 +0100

    v2: changed the main menu with addidng game_status, finish_game_menu functions; bug fixed with start a new game

--- tictactoe.sh
+++ tictactoe.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 # TODO:
 # Task 1: Main function to start the game
@@ -9,44 +10,46 @@
 #current_player = 1
 
 is_game_running=true
+user_choice=""
+computer_choice=""
+
 # declaration of board
 board=(" " " " " "
-         " " " " " "
-         " " " " " ")
+       " " " " " "
+       " " " " " ")
 
-function isGameStopped() {
-    echo "Hello from is Game stooped"
-    # Placeholder for game status check logic
-}
-
-function start_input_verification() {
+# main function to choose the char to game with verification of user input
+function game_menu() {
     while true; do
-        read -p "Please choose the game character (X or O): " user_choice
-        if [[ $user_choice = 'X' || $user_choice = 'O' ]]; then
-            if [[ $user_choice == 'X' ]]; then
-                user_player=1
-                computer_player=2
-            else
-                user_player=2
-                computer_player=1
-            fi
-            echo "Your choice is: $user_choice"
-            echo "User_player: $user_player"
-            echo "Computer_player: $computer_player"
+        echo "======================================="
+        echo "==============='MAIN MENU'=============="
+        echo "To stop and save the game press   -> save"
+        echo "To finish the game press          -> end"
+        echo "======================================="
 
-            # break from while loop
+        read -p "Please choose the game character (X or O): " user_choice
+        if [[ $user_choice = "X" || $user_choice = "O" ]]; then
             break
-        else
+        else 
             echo "Please use only X or O option to be able to play :)"
         fi
     done
+
+    if [[ $user_choice == "X" ]]; then
+        computer_choice="O"
+    else 
+        computer_choice="X"
+    fi
+    echo "User choice: $user_choice and computer: $computer_choice"
 }
 
-
+# To draw the first replica of draw board to play (to see the board)
 function draw_board() {
 
-    # Placeholder for drawing the game board
-    echo "draw the board ..."
+    for i in ${!board[@]}; do 
+        board[$i]=" "
+    done
+    echo "Drawing the initial board ..."
     echo "-------------"
     echo "| ${board[0]} | ${board[1]} | ${board[2]} |"
     echo "------------"
@@ -56,11 +59,8 @@ function draw_board() {
     echo "-------------"
 }
 
-function game_menu() {
-    # Placeholder for game menu logic
-    echo "Hello from game_menu"
-}
-function position_input(){
+# User move function with verification of input
+function player_move(){
     while $is_game_running; do
         read -p "Position: " position
 
@@ -73,44 +73,35 @@ function position_input(){
             echo "Invalid position"
             continue
         fi
+        #verify if the cell is empty
         if [[ ${board[$position]} == " " ]]; then 
-            if [[ $user_player == 1 ]]; then
-                board[$position]='X'
-            else
-                board[$position]='O'
-            fi
+            board[$position]=$user_choice
             update_board
-            
+            break
         else
             echo "Position is taken, please use another"
         fi
     done
-
 }
 
+# computer move
 function computer_move() { 
     #which cells are empy ?
-    for i in ${board[@]}; do
+    echo "Computer's move..."
+
+    for i in "${!board[@]}"; do
         if [[ ${board[$i]} == " " ]]; then
-            if [[ $computer_player == 1 ]]; then 
-                board[$i]='X'
-            else 
-                board[$i]='O'
-            fi
-            update_board
-            
+            board[$i]=$computer_choice
+            echo "Computer placed $computer_choice in position $i."
+            update_board  # Update the board after the computer's move 
+            return  # Exit the function after making the move          
         fi
     done
-
-    #for i in ${!board[@]}; do 
-     #   if [[ ${board[$i]} == " " ]]; then
-      #      board[$position]=$computer_player
-       #     update_board
-        #    break
-        #fi 
-    #done
 }
+
+
 function update_board(){
+    
     echo "Updating..."
     echo "-------------"
     echo "| ${board[0]} | ${board[1]} | ${board[2]} |"
@@ -120,16 +111,45 @@ function update_board(){
     echo "| ${board[6]} | ${board[7]} | ${board[8]} |"
     echo "-------------"
 
+    game_status
+}
+
+
+function game_status() {
+    #for i in ${board[@]}; do
+    echo "Checking game status..."
+    for i in "${!board[@]}"; do
+        if [[ ${board[$i]} == " " ]]; then
+            return
+        fi
+    done
+    is_game_running=false
+    echo "We have the winner, so the game is stopped"
+    finish_game_menu
+}
+
+# function to play one more time after finish the game
+function finish_game_menu() {
+    read -p "To play one more time, press: 1, for no press 2: " finish_menu_input
+    if [[ $finish_menu_input == 1 ]]; then 
+        is_game_running=true
+        main
+    fi
+    exit
 }
 
 #main start game function
 function main() {
     echo "Hello, this is the TicTacToe terminal game!"
-    start_input_verification
+    game_menu
     draw_board
-    position_input
+    # while is_game_running = true
+    while $is_game_running; do 
+        computer_move
+        player_move
+    done
+    finish_game_menu
 }
 
 # Start the main function
-
 main
